2364c55d5c8b35460d347f1957d71218508cd7b2
King-Z-101 <111461124+King-Z-101@users.noreply.github.com>
2023-01-31 21:51:05 -0800

    Update queue_tests.py

--- Lab3/queue_tests.py
+++ Lab3/queue_tests.py
@@ -8,10 +8,22 @@ class TestLab1(unittest.TestCase):
         q = Queue(5)  
         self.assertEqual(q.is_empty(), True)  
         self.assertEqual(q.is_full(), False)  
-        q.enqueue('thing')
-        q.enqueue('that')
-        # q.dequeue()  
-        self.assertEqual(q.size(), 2)  
+        q.enqueue(1)
+        q.enqueue(2)
+        q.enqueue(3)
+        q.enqueue(4)
+        q.enqueue(5)
+        self.assertEqual(q.size(), 5)
+        with self.assertRaises(IndexError):
+            q.enqueue(6)
+        self.assertEqual(q.dequeue(), 1)
+        self.assertEqual(q.size(), 4)
+        self.assertEqual(q.head.item, 2)
+        self.assertEqual(q.tail.item, 5)
+        self.assertEqual(q.dequeue(), 2)
+        self.assertEqual(q.head.item, 3)
+        self.assertEqual(q.tail.item, 5)
+  
    
 if __name__ == '__main__':   
     unittest.main() 
